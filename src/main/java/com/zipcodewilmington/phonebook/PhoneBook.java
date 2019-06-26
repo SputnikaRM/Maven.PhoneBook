fb454035a24b9fb243599a285f597a547fac54fe
sputnika <sputnika91@gmail.com>
2019-06-26 14:20:03 -0400

    done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,18 +1,16 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
     Map<String, List<String>> map;
-    ArrayList <String> phoneNumbers = new ArrayList<>();
+   List <String> list = new ArrayList<>();
 
     public PhoneBook(Map<String, List<String>> map) {this.map=map;
+
     }
 
     public PhoneBook() { map= new LinkedHashMap<>();
@@ -20,28 +18,50 @@ public class PhoneBook {
     }
 
     public void add(String name, String phoneNumber) {
+        if(map.containsKey(name))
+            map.get(name).add(phoneNumber);
+        else{map.put(name,list);
+            add(name,phoneNumber);}
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
+
+        map.put(name, Arrays.asList(phoneNumbers));
+
+
     }
 
     public void remove(String name) {
+
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+
+        if( map.containsKey(name))
+            return true;
+        return false;
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        String s="";
+        for(Map.Entry<String,List<String>> entry : map.entrySet())
+//            if(entry.getValue().equals(phoneNumber))
+            s= entry.getKey();
+        return s;
+
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        ArrayList<String> names = new ArrayList<>();
+        for(String s : map.keySet())
+            names.add(s);
+        return names;
     }
 
     public Map<String, List<String>> getMap() {
